677b50de8c72ac20b777d8cf15c93f1514e5721c
guzheyuan <guzheyuan1991@163.com>
2019-03-03 23:49:52 +0800

    Update install_ssserver.sh

--- install_ssserver.sh
+++ install_ssserver.sh
@@ -1,11 +1,15 @@
 # 安装pip
 yum install -y  python-setuptools && easy_install pip
 
+# 安装libsodium
+yum install epel-release -y
+yum install libsodium -y
+
 # 安装ss服务
-pip install shadowsocks
+pip install https://github.com/shadowsocks/shadowsocks/archive/master.zip -U
 
 # 生成配置文件
-echo '{"server":"0.0.0.0","port_password": {"753":"29574113"},"method": "aes-256-cfb"}' > /etc/shadowsocks.json
+echo '{"server":"0.0.0.0","port_password": {"5081":"xxxxxxxxx"},"method": "chacha20-ietf-poly1305"}' > /etc/shadowsocks.json
 
 # 开启服务
 ssserver -c /etc/shadowsocks.json -d start
